e7efb0e7ac9f8951c7dd4145792206874c7e2df3
williamvidal87 <williamvidal09@yahoo.com>
2023-10-30 23:15:32 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return redirect()->route('login');
+    return view('welcome');
+    // return redirect()->route('login');
 });
 
 Route::middleware([
