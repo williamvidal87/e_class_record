c4b09bb5e3ba6e124754225515f6644257f88622
williamvidal87 <williamvidal09@yahoo.com>
2023-11-01 22:46:07 +0800

    pushing update

--- resources/views/livewire/profile/edit-password.blade.php
+++ resources/views/livewire/profile/edit-password.blade.php
@@ -10,31 +10,30 @@
             <div class="content-body modal-footer justify-content-center">
                 <!-- Zero configuration table -->
                     <!-- Predefined Views -->
-                    
-                    <div class="card col-xl-8 col-lg-6 col-md-12">
-                        <div class="card-body border-top-blue-grey border-top-lighten-5">
-                            <!-- contacts view -->
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="current">Current Password</label>
-                                    <input type="password" wire:model="current" id="current" class="form-control" placeholder="Current Password">
-                                    <div class="font-small-2 danger">@error('current') {{ $message }} @enderror</div>
+                        <div class="card col-xl-8 col-lg-6 col-md-12">
+                            <div class="card-body border-top-blue-grey border-top-lighten-5">
+                                <!-- contacts view -->
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="current_password">Current Password</label>
+                                        <input type="password" wire:model="state.current_password" id="current_password" class="form-control" placeholder="Current Password">
+                                        <div class="font-small-2 danger">@error('current_password') {{ $message }} @enderror</div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">New Password</label>
+                                        <input type="password" wire:model="state.password" id="password" class="form-control" placeholder="New Password">
+                                        <div class="font-small-2 danger">@error('password') {{ $message }} @enderror</div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="confirm_password">Confirm Password</label>
+                                        <input type="password" wire:model="state.password_confirmation" id="confirm_password" class="form-control" placeholder="Confirm Password">
+                                        <div class="font-small-2 danger">@error('confirm_password') {{ $message }} @enderror</div>
+                                    </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button wire:click="updatePassword" type="button" class="btn btn-primary"><i class="la la-check-square-o"></i> Save</button>
                                 </div>
-                                <div class="form-group">
-                                    <label for="password">New Password</label>
-                                    <input type="password" wire:model="password" id="password" class="form-control" placeholder="New Password">
-                                    <div class="font-small-2 danger">@error('password') {{ $message }} @enderror</div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="confirm_password">Confirm Password</label>
-                                    <input type="password" wire:model="confirm_password" id="confirm_password" class="form-control" placeholder="Confirm Password">
-                                    <div class="font-small-2 danger">@error('confirm_password') {{ $message }} @enderror</div>
-                                </div>
-                            <div class="modal-footer justify-content-between">
-                                <button wire:click="Store" type="button" class="btn btn-primary"><i class="la la-check-square-o"></i> Save</button>
                             </div>
                         </div>
-                    </div>
                     <!--/ Predefined Views -->
             </div>
         </div>
