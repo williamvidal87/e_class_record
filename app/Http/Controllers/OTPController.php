c013f081035ae197b3706794476d9c196e394629
williamvidal87 <williamvidal09@yahoo.com>
2024-05-16 21:36:26 +0800

    push update

--- app/Http/Controllers/OTPController.php
+++ app/Http/Controllers/OTPController.php
@@ -38,31 +38,51 @@ class OTPController extends Controller
 
     public function sendOTP()
     {
-        // $otp = rand(100000, 999999);
-        $otp = 123456;
+        $otp = rand(100000, 999999);
+        // $otp = 123456;
         session(['otp_code' => $otp]);
-
         $user = Auth::user();
         $phone = $user->phone_number;
-
-        $configuration = new Configuration(
-            host: 'https://89ljgr.api.infobip.com',
-            apiKey: '1079dd4ca6061011c676b31082f71093-5dc7cff6-784a-4f7d-be9e-02c73760068e'
+        // $configuration = new Configuration(
+        //     host: 'https://89ljgr.api.infobip.com',
+        //     apiKey: '1079dd4ca6061011c676b31082f71093-5dc7cff6-784a-4f7d-be9e-02c73760068e'
+        // );
+        // $sendSmsApi = new SmsApi(config: $configuration);
+        // $message = new SmsTextualMessage(
+        //     destinations: [
+        //         new SmsDestination(to: '63'.$phone)
+        //     ],
+        //     from: 'E-class',
+        //     text: "E-Class: ".$otp." is your security code. Don't share your code."
+        // );
+        // // $request = new SmsAdvancedTextualRequest(messages: [$message]);
+        // try {
+        //     // $smsResponse = $sendSmsApi->sendSmsMessage($request);
+        // } catch (ApiException $apiException) {
+        //     // dd($apiException);
+        // }
+        
+        $apiSecret = "8674b65c85d896de6b4cfa89f6670546163be261";
+        $deviceId = "00000000-0000-0000-3229-b4d8b3f7a96f";
+        $phone = "+63".$user->phone_number;
+        $messageContent = "E-Class: ".$otp." is your security code. Don't share your code.";
+        $messageData = array(
+            "secret" => $apiSecret,
+            "mode" => "devices",
+            "device" => $deviceId,
+            "sim" => 2,
+            "priority" => 1,
+            "phone" => $phone,
+            "message" => $messageContent
         );
-        $sendSmsApi = new SmsApi(config: $configuration);
-        $message = new SmsTextualMessage(
-            destinations: [
-                new SmsDestination(to: '63'.$phone)
-            ],
-            from: 'E-class',
-            text: "E-Class: ".$otp." is your security code. Don't share your code."
-        );
-        // $request = new SmsAdvancedTextualRequest(messages: [$message]);
-        try {
-            // $smsResponse = $sendSmsApi->sendSmsMessage($request);
-        } catch (ApiException $apiException) {
-            dd($apiException);
-        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.cloud.smschef.com/api/send/sms");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($messageData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($response, true);
 
         return redirect()->route('otp.verify');
     }
--- app/Livewire/InstructorPanel/ClassRecord/ViewClassRecordForm.php
+++ app/Livewire/InstructorPanel/ClassRecord/ViewClassRecordForm.php
@@ -171,24 +171,46 @@ Best regards,
 ".$this->InstructorName;
 
                 if($check_has_final!=0||$check_has_mid!=0){
-                    $configuration = new Configuration(
-                        host: 'https://89ljgr.api.infobip.com',
-                        apiKey: '1079dd4ca6061011c676b31082f71093-5dc7cff6-784a-4f7d-be9e-02c73760068e'
+                    // $configuration = new Configuration(
+                    //     host: 'https://89ljgr.api.infobip.com',
+                    //     apiKey: '1079dd4ca6061011c676b31082f71093-5dc7cff6-784a-4f7d-be9e-02c73760068e'
+                    // );
+                    // $sendSmsApi = new SmsApi(config: $configuration);
+                    // $message = new SmsTextualMessage(
+                    //     destinations: [
+                    //         new SmsDestination(to: '63'.$StudentName->phone_number)
+                    //     ],
+                    //     from: 'E-class',
+                    //     text: $message
+                    // );
+                    // // $request = new SmsAdvancedTextualRequest(messages: [$message]);
+                    // try {
+                    //     // $smsResponse = $sendSmsApi->sendSmsMessage($request);
+                    // } catch (ApiException $apiException) {
+                    //     // dd($apiException);
+                    // }
+
+                    $apiSecret = "8674b65c85d896de6b4cfa89f6670546163be261";
+                    $deviceId = "00000000-0000-0000-3229-b4d8b3f7a96f";
+                    $phone = '+639212969669';
+                    $messageContent = $message;
+                    $messageData = array(
+                        "secret" => $apiSecret,
+                        "mode" => "devices",
+                        "device" => $deviceId,
+                        "sim" => 2,
+                        "priority" => 1,
+                        "phone" => $phone,
+                        "message" => $messageContent
                     );
-                    $sendSmsApi = new SmsApi(config: $configuration);
-                    $message = new SmsTextualMessage(
-                        destinations: [
-                            new SmsDestination(to: '63'.$StudentName->phone_number)
-                        ],
-                        from: 'E-class',
-                        text: $message
-                    );
-                    // $request = new SmsAdvancedTextualRequest(messages: [$message]);
-                    try {
-                        // $smsResponse = $sendSmsApi->sendSmsMessage($request);
-                    } catch (ApiException $apiException) {
-                        // dd($apiException);
-                    }
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, "https://www.cloud.smschef.com/api/send/sms");
+                    curl_setopt($ch, CURLOPT_POST, true);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($messageData));
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    $response = curl_exec($ch);
+                    curl_close($ch);
+                    $result = json_decode($response, true);
                 }
 
             }
