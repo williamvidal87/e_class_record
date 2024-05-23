7ca8709e91a335fcee918f81859369dfd555fe9a
williamvidal87 <williamvidal09@yahoo.com>
2024-05-23 15:53:15 +0800

    new update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            OtherUserSeeder::class,
             SubjectSeeder::class,
             SemesterSeeder::class,
             CourseSeeder::class,
